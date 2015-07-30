5df036ef24d42f4e150b3515c97b9bce8ed9633a
Jake-Harris9001 <jakeharris9001@gmail.com>
2015-07-30 12:46:49 +1000

    Handled program exiting, added user confirmation box.

---
+++ Main/src/main/java/tb/ConfirmBox.java
@@ -0,0 +1,57 @@
+package tb;
+
+/**
+ * Created by JakeSuper on 30/07/2015.
+ */
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ * Created by JakeSuper on 30/07/2015.
+ */
+public class ConfirmBox {
+    static boolean answer;
+
+    public static boolean display(String title, String message){
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(250);
+
+
+        //Set message on the label
+        Label label = new Label();
+        label.setText(message);
+
+        //Set the buttons up
+        Button yesButton = new Button("Yes");
+        Button noButton = new Button("No");
+
+        yesButton.setOnAction(e -> {
+            answer = true;
+            window.close();
+        });
+        noButton.setOnAction(e -> {
+            answer = false;
+            window.close();
+        });
+
+        HBox layout2 = new HBox(30);
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, layout2);
+        layout2.getChildren().addAll(yesButton, noButton);
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+        return answer;
+    }
+
+
+
+}
